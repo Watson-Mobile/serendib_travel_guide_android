ed71b99734a1d87fef62be66ba1250d09e41590a
supimi <supimi.15@cse.mrt.ac.lk>
2020-01-03 17:39:51 +0530

    Material theme added

--- app/src/main/java/com/watson/serendibtravelguide/ui/Utils/BottomNavigationViewHelper.java
+++ app/src/main/java/com/watson/serendibtravelguide/ui/Utils/BottomNavigationViewHelper.java
@@ -1,6 +1,8 @@
 package com.watson.serendibtravelguide.ui.Utils;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.MenuItem;
 
@@ -8,14 +10,18 @@ import androidx.annotation.NonNull;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.watson.serendibtravelguide.R;
+import com.watson.serendibtravelguide.ui.home.HomeActivity;
+import com.watson.serendibtravelguide.ui.search.SearchActivity;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class BottomNavigationViewHelper {
-    private static final String TAG = "BottomNavigationViewHel";
+ //   private static final String TAG = "BottomNavigationViewHelper";
 
 
-    public static void setupBottomNavigationView(BottomNavigationView bottomNavigationView) {
+    public static void setupBottomNavigationView(final Context context, BottomNavigationView bottomNavigationView) {
 //        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavBar);
-        Log.d(TAG, "setupBottomNavigationView: Setting up BottomNavigationView");
+//        Log.d(TAG, "setupBottomNavigationView: Setting up BottomNavigationView");
 
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -24,9 +30,17 @@ public class BottomNavigationViewHelper {
                         switch (item.getItemId()) {
                             case R.id.nav_home:
                                 //launch home activity
+//                                Log.d(TAG, "Case Home");
+//                                Intent intent1 = new Intent(context, HomeActivity.class);
+
+//                                context.startActivity(intent1);
                                 break;
                             case R.id.nav_search:
                                     // search activity
+//                                Log.d(TAG, "Case Search Isuruuu");
+//                                Intent intent = new Intent(context, SearchActivity.class);
+
+//                                context.startActivity(intent);
                                 break;
                             case R.id.nav_notifications:
                                 //add activity
--- app/src/main/java/com/watson/serendibtravelguide/ui/home/HomeActivity.java
+++ app/src/main/java/com/watson/serendibtravelguide/ui/home/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.watson.serendibtravelguide.ui.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -11,17 +13,22 @@ import com.watson.serendibtravelguide.ui.Utils.BottomNavigationViewHelper;
 
 public class HomeActivity extends AppCompatActivity {
     private static final String TAG = "HomeActivity";
+    private Context mContext = HomeActivity.this;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setupBottomNavigationView();
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+//        setSupportActionBar(toolbar);
     }
 
     private void setupBottomNavigationView(){
         Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
         BottomNavigationView bottomNavigationViewEx = (BottomNavigationView) findViewById(R.id.bottomNavBar);
-        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewHelper.setupBottomNavigationView(mContext, bottomNavigationViewEx);
     }
 }
