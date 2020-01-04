239564bb419c136024d3dd57cdaf6e6e1a9c0a04
piyumisudusinghe <piyumisudusinghe@gmail.com>
2020-01-04 15:12:35 +0530

    added backend calls

--- app/src/main/java/com/watson/serendibtravelguide/data/LoginDataSource.java
+++ app/src/main/java/com/watson/serendibtravelguide/data/LoginDataSource.java
@@ -1,14 +1,36 @@
 package com.watson.serendibtravelguide.data;
 
+import android.util.Log;
+
+import com.watson.serendibtravelguide.BuildConfig;
 import com.watson.serendibtravelguide.data.model.User;
+import com.watson.serendibtravelguide.data.model.UserResponse;
+import com.watson.serendibtravelguide.rest.UserApiService;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class LoginDataSource {
-    public Result<User> login(String username, String password) {
+
+
+    public static final String BASE_URL_AWS = "http://ec2-34-238-82-190.compute-1.amazonaws.com/api/";
+    private static Retrofit retrofit = null;
+
+    private final static String API_KEY = BuildConfig.CONSUMER_KEY;
+    public Result<User> login(String email, String password) {
 
         try {
+
+            connectAndGetApiDataAWS(email);
             // TODO: handle loggedInUser authentication
             ArrayList<String> guide_locations = new ArrayList<>();
             guide_locations.add("Matara");
@@ -25,5 +47,37 @@ public class LoginDataSource {
 
     public void logout() {
         // TODO: revoke authentication
+    }
+
+    public void connectAndGetApiDataAWS(String email) {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL_AWS)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        UserApiService userApiService = retrofit.create(UserApiService.class);
+
+        List<User> user = new ArrayList<>();
+
+
+        Call<UserResponse> call = userApiService.getUserByEmail(email);
+        call.enqueue(new Callback<UserResponse>() {
+            @Override
+            public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
+                List<User> user = response.body().getData();
+//                places = response.body().getData();
+//                recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
+
+                Log.d(TAG, "Number of users received: " + user.size());
+                Log.d("USER_REQUEST","users output" + user);
+
+            }
+
+            @Override
+            public void onFailure(Call<UserResponse> call, Throwable throwable) {
+                Log.e(TAG, throwable.toString());
+            }
+        });
     }
 }
--- app/src/main/java/com/watson/serendibtravelguide/ui/Register/RegisterLocalGuideActivity.java
+++ app/src/main/java/com/watson/serendibtravelguide/ui/Register/RegisterLocalGuideActivity.java
@@ -1,17 +1,37 @@
 package com.watson.serendibtravelguide.ui.Register;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.watson.serendibtravelguide.R;
+import com.watson.serendibtravelguide.ui.userlogin.LoggedUserView;
+import com.watson.serendibtravelguide.ui.userlogin.LoginFormState;
+import com.watson.serendibtravelguide.ui.userlogin.LoginResult;
+import com.watson.serendibtravelguide.ui.userlogin.LoginViewModel;
+import com.watson.serendibtravelguide.ui.userlogin.LoginViewModelFactory;
 
 public class RegisterLocalGuideActivity extends AppCompatActivity {
 
     Button btn_nextPage;
+
+    RegisterViewModel registerViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,9 +44,110 @@ public class RegisterLocalGuideActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
+
+        registerViewModel = ViewModelProviders.of(this, new RegisterViewModelFactory())
+                .get(RegisterViewModel.class);
+
+        final EditText firstname = findViewById(R.id.firstname);
+        final EditText lastname = findViewById(R.id.lastname);
+        final EditText username = findViewById(R.id.username);
+        final EditText nic_number = findViewById(R.id.nic_no);
+        final EditText telephone_number = findViewById(R.id.tel_no);
+        final EditText  email = findViewById(R.id.email);
+        final EditText password = findViewById(R.id.register_pwd);
+        final EditText confirm_password = findViewById(R.id.register_con_pwd);
+        final Button nextButton = findViewById(R.id.Next);
+        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+
+//        registerViewModel.getRegisterFormState().observe(this, new Observer<RegisterFormState>() {
+//            @Override
+//            public void onChanged(@Nullable LoginFormState loginFormState) {
+//                if (loginFormState == null) {
+//                    return;
+//                }
+//                loginButton.setEnabled(loginFormState.isDataValid());
+//                if (loginFormState.getUsernameError() != null) {
+//                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
+//                }
+//                if (loginFormState.getPasswordError() != null) {
+//                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
+//                }
+//            }
+//        });
+
+//        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
+//            @Override
+//            public void onChanged(@Nullable LoginResult loginResult) {
+//                if (loginResult == null) {
+//                    return;
+//                }
+//                loadingProgressBar.setVisibility(View.GONE);
+//                if (loginResult.getError() != null) {
+//                    showLoginFailed(loginResult.getError());
+//                }
+//                if (loginResult.getSuccess() != null) {
+//                    updateUiWithUser(loginResult.getSuccess());
+//                }
+//                setResult(Activity.RESULT_OK);
+//
+//                //Complete and destroy login activity once successful
+//                finish();
+//            }
+//        });
+
+//        TextWatcher afterTextChangedListener = new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//                // ignore
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                // ignore
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
+//                        passwordEditText.getText().toString());
+//            }
+//        };
+//        usernameEditText.addTextChangedListener(afterTextChangedListener);
+//        passwordEditText.addTextChangedListener(afterTextChangedListener);
+//        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//
+//            @Override
+//            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+//                if (actionId == EditorInfo.IME_ACTION_DONE) {
+//                    loginViewModel.login(usernameEditText.getText().toString(),
+//                            passwordEditText.getText().toString());
+//                }
+//                return false;
+//            }
+//        });
+//
+//        loginButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                loadingProgressBar.setVisibility(View.VISIBLE);
+//                loginViewModel.login(usernameEditText.getText().toString(),
+//                        passwordEditText.getText().toString());
+//            }
+//        });
+//    }
+//
+//    private void updateUiWithUser(LoggedUserView model) {
+//        String welcome = getString(R.string.welcome) + model.getDisplayName();
+//        // TODO : initiate successful logged in experience
+//        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+//    }
+//
+//    private void showLoginFailed(@StringRes Integer errorString) {
+//        Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
+//    }
+//    }
 
 
 
 
-}
+}}
