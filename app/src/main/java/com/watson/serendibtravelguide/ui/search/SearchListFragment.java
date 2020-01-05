5ec9fd23cf3ff7af78538b3d29d97d742e980524
supimi <supimi.15@cse.mrt.ac.lk>
2020-01-06 00:04:55 +0530

    search fragment connected with backend calls

--- app/src/main/java/com/watson/serendibtravelguide/ui/search/SearchListFragment.java
+++ app/src/main/java/com/watson/serendibtravelguide/ui/search/SearchListFragment.java
@@ -1,10 +1,12 @@
 package com.watson.serendibtravelguide.ui.search;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,38 +14,110 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.watson.serendibtravelguide.R;
+import com.watson.serendibtravelguide.config.Config;
+import com.watson.serendibtravelguide.models.Place;
+import com.watson.serendibtravelguide.models.PlaceResponse;
+import com.watson.serendibtravelguide.rest.PlaceApiService;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class SearchListFragment extends Fragment {
-    private SearchViewModel searchViewModel;
-    ArrayAdapter<String> adapter;
-    List<SearchViewModel> test_search_results = new ArrayList<>();
-    RecyclerView recyclerView;
-    RecycleViewAdapterSearch searchAdapter;
+    private static final String TAG = "SearchListFragment";
+    private static Retrofit retrofit = null;
+    private String query;
+    private List<Place> searchPlaceList = new ArrayList<>();
+    private List<SearchViewModel> searchViewList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private RecycleViewAdapterSearch searchAdapter;
+    private TextView searchResultsTextView;
 
-    public SearchListFragment(){
 
+    public SearchListFragment(String query) {
+        this.query = query;
+    }
+
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(Config.serverIp)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        Log.d(TAG, "From search fragment, query received: " + query);
+        this.queryPlaces(query);
+
+
         View root = inflater.inflate(R.layout.fragment_search_list, container, false);
-
-        test_search_results.add(new SearchViewModel("search1","tag1"));
-        test_search_results.add(new SearchViewModel("search2","tag1,2"));
-
+        searchResultsTextView = root.findViewById(R.id.search_results_title);
         recyclerView = root.findViewById(R.id.search_result_list);
-        searchAdapter = new RecycleViewAdapterSearch(test_search_results);
+        searchAdapter = new RecycleViewAdapterSearch(searchViewList, this.getContext());
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this.getActivity());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(searchAdapter);
+
         return root;
     }
 
 
+    public void queryPlaces(String query) {
+        PlaceApiService placeApiService = retrofit.create(PlaceApiService.class);
+
+        Call<PlaceResponse> searchCall = placeApiService.searchPlaces(query);
+
+        searchCall.enqueue(new Callback<PlaceResponse>() {
+            @Override
+            public void onResponse(Call<PlaceResponse> call, Response<PlaceResponse> response) {
+                List<Place> searchPlaces = response.body().getData();
+                Log.d(TAG, response.body().getMessage());
+
+                for (Place place : searchPlaces) {
+                    searchPlaceList.add(place);
+                    String secondaryTitle = "";
+                    for (String name : place.getOtherNames()) {
+                        secondaryTitle = secondaryTitle.concat(name).concat(" ");
+                    }
+
+                    SearchViewModel searchViewModel = new SearchViewModel(place.getName(), place.getType().get(0),
+                            secondaryTitle, place.getImagePaths().get(0));
+                    searchViewList.add(searchViewModel);
+
+                }
+                if (searchViewList.isEmpty()){
+                    searchResultsTextView.setText("No results found for \'"+query+"\'");
+                    Log.d(TAG,"No results");
+                }else{
+                    Log.d(TAG,"Search results found");
+                    searchResultsTextView.setText("Search Results...");
+                }
+
+                searchAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onFailure(Call<PlaceResponse> call, Throwable t) {
+
+            }
+        });
+    }
 
 
 }
--- app/src/main/java/com/watson/serendibtravelguide/ui/search/SearchViewModel.java
+++ app/src/main/java/com/watson/serendibtravelguide/ui/search/SearchViewModel.java
@@ -1,17 +1,18 @@
 package com.watson.serendibtravelguide.ui.search;
 
-import android.widget.TextView;
-
 import androidx.lifecycle.ViewModel;
 
 public class SearchViewModel extends ViewModel {
     private String cardTitle;
     private String cardTag;
-   // private String
+    private String otherNames;
+    private String image;
 
-    public SearchViewModel(String cardTitle, String cardTag) {
+    public SearchViewModel(String cardTitle, String cardTag,String otherNames, String image) {
         this.cardTitle = cardTitle;
         this.cardTag = cardTag;
+        this.otherNames = otherNames;
+        this.image=image;
     }
 
     public String getCardTitle() {
@@ -28,5 +29,21 @@ public class SearchViewModel extends ViewModel {
 
     public void setCardTag(String cardTag) {
         this.cardTag = cardTag;
+    }
+
+    public String getOtherNames() {
+        return otherNames;
+    }
+
+    public void setOtherNames(String otherNames) {
+        this.otherNames = otherNames;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
 }
