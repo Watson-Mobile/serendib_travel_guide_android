8004ed6c7bfe38281c728f07f87b36d4b03111a8
piyumisudusinghe <piyumisudusinghe@gmail.com>
2020-01-05 20:11:30 +0530

    chnaged map file

--- app/src/main/java/com/watson/serendibtravelguide/ui/Register/MapsActivity.java
+++ app/src/main/java/com/watson/serendibtravelguide/ui/Register/MapsActivity.java
@@ -5,6 +5,11 @@ import androidx.fragment.app.FragmentActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.watson.serendibtravelguide.R;
 import com.google.android.gms.common.api.Status;
 import com.google.android.libraries.places.api.Places;
@@ -15,15 +20,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.model.Place;
+
 import com.google.android.libraries.places.widget.AutocompleteFragment;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 import static androidx.constraintlayout.widget.Constraints.TAG;
+
+import com.google.android.libraries.places.api.Places;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -66,24 +76,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Places.initialize(getApplicationContext(), "AIzaSyDflkvLylyshUs_Qs-7U_9gvWbqgWReijE");
         }
 
-        // Initialize the AutocompleteSupportFragment.
+// Initialize the AutocompleteSupportFragment.
         AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
                 getSupportFragmentManager().findFragmentById(R.id.place_autocomplete);
 
-        autocompleteFragment.setPlaceFields(Arrays.asList( Place.Field.LAT_LNG, Place.Field.NAME));
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
 
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
                 // TODO: Get info about the selected place.
-                Log.i(TAG, "Place: " + place.getName() + ", " + place.getLatLng());
-                if(place != null){
-                    lat = Objects.requireNonNull(place.getLatLng()).latitude;
-                    longi = place.getLatLng().latitude;
-                    place_name = place.getName();
-              }
-
-
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
             }
 
             @Override
@@ -92,28 +95,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Log.i(TAG, "An error occurred: " + status);
             }
         });
-
-
-
-
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng new_place = new LatLng(lat,longi);
-        mMap.addMarker(new MarkerOptions().position(new_place).title(place_name));
+        LatLng new_place = new LatLng(45,78);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(new_place));
 
 
